0fbcd2b299137e9a3efb5734315ec989c8592546
M <manelruizblas@gmail.com>
2023-03-16 11:26:49 +0100

    Update tests.py
    Updating code

--- tests/tests.py
+++ tests/tests.py
@@ -5,4 +5,24 @@ from pages.search import SydsvenskanSearch
 
 
 @pytest.mark.parametrize("words", ["kärlek"])
-def
+def test_search_sydsvenskan(browser, words):
+    search_page = SydsvenskanSearch(browser)
+    result_page = SydsvenskanResult(browser)
+
+    # Loading the home page
+    search_page.load()
+
+    # Clicks on the search button
+    search_page.open_search()
+
+    # The search words are introduced
+    search_page.search(words)
+
+    # Check if the words search on the searh page
+    assert words == result_page.search_input_value()
+    assert words == result_page.title()
+
+
+def test_section_sydsvenskan(browser):
+    pass
+
